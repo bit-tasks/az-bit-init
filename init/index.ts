fc2fe35ac1f523f8a9422e54c83dd71ec0224a58
ashanfernando <ashan256@gmail.com>
2023-07-20 23:37:55 +0530

    Update BIT_TOKEN variable

--- init/index.ts
+++ init/index.ts
@@ -4,9 +4,9 @@ import runInit from './scripts/init';
 async function run() {
     try {
         const wsdir: string | undefined = task.getInput('wsdir', false) || './';
-        const bitToken = process.env.BIT_TOKEN;
+        const bitToken = process.env.BIT_CONFIG_USER_TOKEN;
         if (!bitToken) {
-            task.setResult(task.TaskResult.Failed, 'BIT_TOKEN environment variable is not set');
+            task.setResult(task.TaskResult.Failed, 'BIT_CONFIG_USER_TOKEN environment variable is not set');
             return;
         }
         runInit(bitToken, wsdir);
